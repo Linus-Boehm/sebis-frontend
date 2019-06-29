3cc24974fdc3f16e80d6d482d067b2a0e42c8496
Linus Boehm <letters@linusboehm.de>
2019-06-29 19:11:53 +0200

    basic user reducer

--- store/reducers/index.js
+++ store/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth';
 import teamReducer from './teams';
+import userReducer from './users';
 
 
 export default combineReducers({
     teams: teamReducer,
-    auth: authReducer
+    auth: authReducer,
+    users: userReducer
 })
