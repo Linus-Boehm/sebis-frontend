76e123e7c31f6b94b6c2852b58b7b46e3e044525
Anne Seeser <anne.seeser@googlemail.com>
2019-06-29 17:29:51 +0200

    Delete Team

--- store/reducers/teams.js
+++ store/reducers/teams.js
@@ -1,32 +1,44 @@
-import {ASSIGN_TEAMS, ASSIGN_TEAM, RESET_TEAM} from '../types/team'
-import {keyBy} from "lodash";
+import {
+  ASSIGN_TEAMS,
+  ASSIGN_TEAM,
+  RESET_TEAM,
+  DELETE_TEAM
+} from "../types/team";
+import { keyBy } from "lodash";
 
 const initialState = {
-    team: {
-        name: ""
-    },
-    teamList: {}
+  team: {
+    name: ""
+  },
+  teamList: {}
 };
-export default (state = initialState, {type, data}) => {
-    console.log("reducer:teams:"+type);
-    switch (type) {
-        case ASSIGN_TEAMS:
-            return {
-                ...state,
-                teamList: {...state.teamList, ...keyBy(data, '_id')}
-            };
-        case ASSIGN_TEAM:
-            return {
-                ...state,
-                team: {...state.team, ...data}
-            };
-        case RESET_TEAM:
-            return {
-                ...state,
-                team: {...initialState.team}
-            }
-        default:
-            return state;
-    }
+export default (state = initialState, { type, data }) => {
+  console.log("reducer:teams:" + type);
+  switch (type) {
+    case ASSIGN_TEAMS:
+      return {
+        ...state,
+        teamList: { ...state.teamList, ...keyBy(data, "_id") }
+      };
+    case ASSIGN_TEAM:
+      return {
+        ...state,
+        team: { ...state.team, ...data }
+      };
+    case RESET_TEAM:
+      return {
+        ...state,
+        team: { ...initialState.team }
+      };
+    case DELETE_TEAM:
+      let teamListCopy = { ...state.teamList };
+      delete teamListCopy[data];
+      return {
+        ...state,
+        teamList: teamListCopy
+      };
 
-}
+    default:
+      return state;
+  }
+};
