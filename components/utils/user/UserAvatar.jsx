16bd6549a77b024c6c79b96670ecea5042f44852
Daniel Gruber <daniel@ibpg.eu>
2019-07-09 16:53:16 +0200

    - integrate inline editing - add icons for Organization, Team goal

--- components/utils/user/UserAvatar.jsx
+++ components/utils/user/UserAvatar.jsx
@@ -5,24 +5,35 @@ import React from "react";
 class UserAvatar extends React.Component {
 
 
-    render() {
-        const name = this.props.user.firstname + ' ' + this.props.user.lastname;
-        //const colors = ['#D6197F','#D6197F']
+  render() {
+    const classNames = require('classnames');
 
-        return (
-            <div>
-                <Avatar name={name} round={true} size="45" onClick={this.props.onClick} className={this.props.className}>
+    const name = this.props.user ? this.props.user.firstname + ' ' + this.props.user.lastname : "";
+    //const colors = ['#D6197F','#D6197F']
 
-                </Avatar>
-                {/*language=CSS*/
-                }
-                <style jsx global>{`
+    return (
+      <div className={classNames(this.props.className, "inline-block")}>
+        <div className="inline-block relative">
+          <Avatar name={name} value={" "} round={true} size="45" onClick={this.props.onClick}>
 
-                `}</style>
+          </Avatar>
+          <div className="text-white margin-center absolute flex left-0 top-0 bottom-0 right-0">
+            <div className="content-center justify-center m-auto">
+              {
+                this.props.children
+              }
             </div>
-        )
+          </div>
+        </div>
+        {/*language=CSS*/
+        }
+        <style jsx global>{`
 
-    }
+        `}</style>
+      </div>
+    )
+
+  }
 
 }
 
