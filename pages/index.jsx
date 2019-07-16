af5a43cb96fdbc91f23806c941590c36b5ba32fa
Anne Seeser <anne.seeser@googlemail.com>
2019-07-16 11:50:16 +0200

    home

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,42 @@
-import React from 'react';
+import React from "react";
 import DefaultLayout from "../components/layout/DefaultLayout";
 
 class Home extends React.Component {
-
   static async getInitialProps({ req }) {
-    const userAgent = req ? req.headers[ 'user-agent' ] : navigator.userAgent;
+    const userAgent = req ? req.headers["user-agent"] : navigator.userAgent;
     return { userAgent };
   }
 
   render() {
     return (
-      <DefaultLayout>
-        <div className="container">
-          <div>Welcome to Goalify!</div>
-        </div>
-      </DefaultLayout>
-    )
+      <div>
+        <DefaultLayout>
+          <img src="/static/homepage.png" className="imageCover" />
+        </DefaultLayout>
+
+        <style jsx>
+          {`
+          .imageCover {
+            margin-top: -8%;
+            margin-bottom: 20%;
+            z-index:50000
+            margin-left: 30%
+      
+          }
+
+          
+            .backgroundImage {
+             background-image: url("/static/homepage.png");
+              z-index:1
+              background-size:cover;
+              width:100px
+              height: 150%;
+              position: relative;
+            }
+          `}
+        </style>
+      </div>
+    );
   }
 }
 
