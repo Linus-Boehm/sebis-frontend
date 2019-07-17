a364625d308674b52bf9deaecd3c8369fa714fe5
Linus Boehm <letters@linusboehm.de>
2019-07-17 12:51:52 +0200

    Fix react warning

--- components/agreements/list/AgreementItem.jsx
+++ components/agreements/list/AgreementItem.jsx
@@ -6,13 +6,13 @@ class AgreementItem extends React.Component {
   getTag = () => {
     const agreement = this.props.agreement;
     if (agreement.reviewer_confirmed && agreement.assignee_confirmed) {
-      return <span class="tag is-success w-32">Running</span>;
+      return <span className="tag is-success w-32">Running</span>;
     } else if (agreement.end_date < new Date()) {
-      return <span class="tag is-light w-32">Finished</span>;
+      return <span className="tag is-light w-32">Finished</span>;
     } else if (agreement.reviewer_confirmed) {
       return (
         <span
-          class="tag w-32"
+            className="tag w-32"
           style={{ backgroundColor: "#61E8E1", color: "#ffff" }}
         >
           Open Confirmation
@@ -21,14 +21,14 @@ class AgreementItem extends React.Component {
     } else if (agreement.assignee_confirmed) {
       return (
         <span
-          class="tag w-32"
+            className="tag w-32"
           style={{ backgroundColor: "#61E8E1", color: "#ffff" }}
         >
           Open Confirmation
         </span>
       );
     } else {
-      return <span class="tag is-info w-32">Open</span>;
+      return <span className="tag is-info w-32">Open</span>;
     }
   };
 
