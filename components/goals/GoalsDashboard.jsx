0301daa3a5589f2f3bc881fc6f16c785cde5e756
Anne Seeser <anne.seeser@googlemail.com>
2019-07-18 20:26:24 +0200

    Fix bug, Team name instead team_id

--- components/goals/GoalsDashboard.jsx
+++ components/goals/GoalsDashboard.jsx
@@ -1,59 +1,48 @@
-import React from 'react';
-import SearchInput from '../../components/utils/inputs/SearchInput'
+import React from "react";
+import SearchInput from "../../components/utils/inputs/SearchInput";
 import MyGoalsList from "./list/MyGoalsList";
 import TeamGoalsList from "./list/TeamGoalsList";
 import OrganizationGoalsList from "./list/OrganizationGoalsList";
 
-
 class GoalsDashboard extends React.Component {
-
-
   constructor(props) {
     super(props);
     this.state = {
-      searchFilter: ''
+      searchFilter: ""
     };
   }
 
   render() {
-    const {
-      teams,
-    } = this.props;
+    const { teams } = this.props;
 
-    const {
-      searchFilter
-    } = this.state;
+    const { searchFilter } = this.state;
 
     return (
       <div>
         <div className="flex w-full justify-end">
           <SearchInput
             value={this.state.searchFilter}
-            onChange={(e) => {
-              this.setState({ searchFilter: e.target.value })
+            onChange={e => {
+              this.setState({ searchFilter: e.target.value });
             }}
           />
         </div>
         <div>
-          <MyGoalsList
-            searchFilter={searchFilter}
-          />
-          {teams && teams.map(team => (
-            <TeamGoalsList
-              key={"team-" + team._id}
-              title={team._id}
-
-              team={team}
-              searchFilter={searchFilter}
-            />)
-          )}
-          <OrganizationGoalsList
-            searchFilter={searchFilter}
-          />
+          <MyGoalsList searchFilter={searchFilter} />
+          {teams &&
+            teams.map(team => (
+              <TeamGoalsList
+                key={"team-" + team._id}
+                title={team.name}
+                team={team}
+                searchFilter={searchFilter}
+              />
+            ))}
+          <OrganizationGoalsList searchFilter={searchFilter} />
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default GoalsDashboard
+export default GoalsDashboard;
