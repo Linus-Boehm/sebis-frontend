113410ee659e34c1a01e26478e262c79da72a755
Linus Boehm <letters@linusboehm.de>
2019-07-21 15:53:59 +0200

    fix FeedItem aligin

--- components/utils/comment/FeedItem.jsx
+++ components/utils/comment/FeedItem.jsx
@@ -1,37 +1,40 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import UserAvatar from "../user/UserAvatar";
 import moment from "moment";
 
 class FeedItem extends Component {
-  render() {
-    return (
+    render() {
+        return (
 
-      <div className={"flex " + (this.props.className ? this.props.className : '')}>
-        <div className="pr-4">
-          <div className="avatar-placeholder"/>
-        </div>
-        <div className="flex-1">
-          <div className="break-all"
-               style={{ fontSize: 12, fontWeight: "bold" }}> {this.props.comment.text}</div>
-        </div>
-        <div
-          className="flex"
-          style={{ fontSize: 12, color: "#C5C5C5", fontWeight: "bold" }}
-        >
-          <p>{moment(this.props.comment.date).fromNow()}</p>
-        </div>
-        {/*language=CSS*/
-        }
-        <style jsx>{`
-            .avatar-placeholder {
-                width: 45px;
-            }
-        `}</style>
-      </div>
-    );
+            <div className={"flex flex-col " + (this.props.className ? this.props.className : '')}>
+                <div
+                    className="flex"
+                    style={{fontSize: 12, color: "#C5C5C5", fontWeight: "bold"}}
+                >
+                    <p className="ml-auto">{moment(this.props.comment.date).fromNow()}</p>
+                </div>
+                <div className="flex">
+                    <div className="pr-4">
+                        <div className="avatar-placeholder"/>
+                    </div>
+                    <div className="flex-1">
+                        <div className="break-all"
+                             style={{fontSize: 12, fontWeight: "bold"}}> {this.props.comment.text}</div>
+                    </div>
+                </div>
+
+                {/*language=CSS*/
+                }
+                <style jsx>{`
+                    .avatar-placeholder {
+                        width: 45px;
+                    }
+                `}</style>
+            </div>
+        );
 
 
-  }
+    }
 }
 
 export default FeedItem;
